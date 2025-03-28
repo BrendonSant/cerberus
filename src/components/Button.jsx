53c769de53af0b4b3db9efb79258fa5a910dd938
BRENDON M. C. SANTOS <52220998+BrendonSant@users.noreply.github.com>
2025-03-28 15:28:15 -0300

    feat: primeiras implementações do header.

---
+++ src/components/Button.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Button = ({title,id,lefIcon,containerClass}) => {
+  return (
+    <div>{props.title}</div>
+  )
+}
+
+export default Button
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,32 +1,62 @@
 import React from "react";
-
-
+import Button from "./Button";
+import { TiLocationArrow } from "react-icons/ti";
 
 const Hero = () => {
-  return <div className="relative h-dvh w-screen overflow-x-hidden">
-    <div id="video-frame" className="relative h-dvh w-screen overflow-hidden z-10 rounded-lg bg-blue-50">
-      <div>
-        <div className="mask-clip-path absolute-center absolute z-50 size-svh lg:size-full overflow-hidden 
-        rounded-lg">
-          <div>
-           <video
-           src="videos/header_video.mp4"
-           autoPlay
-           loop
-           muted
-           id="current-video"
-           className="size-svh lg:size-full origin-center  object-cover object-center"
-           />
-          </div>
-            <h1 className=" absolute hero-heading text-white bottom-5 right-70 z-40">
-              santsmcb
-            </h1>
+  return (
+    <div className="relative h-screen w-screen overflow-x-hidden">
+      <div
+        id="video-frame"
+        className="relative h-screen w-screen overflow-hidden z-10 bg-blue-50"
+      >
+        <div>
+          <div className="absolute inset-0 z-50 overflow-hidden">
+            <video
+              src="videos/Main_video_1.mp4"
+              //autoPlay
+              loop
+              muted
+              id="current-video"
+              className="absolute inset-0 w-full h-full object-cover"
+            />
+            <h1 className="absolute font-orbitron hero-heading text-white bottom-5 right-10 z-40">
+              {/* Exibido apenas em telas menores que lg */}
+              <span className="block lg:hidden text-left">
+                art <br /> code <br /> design
+              </span>
 
+              {/* Exibido apenas em telas lg e maiores */}
+              <span className="hidden lg:inline">
+                design
+                <br />
+                art/code
+              </span>
+            </h1>
+            <div className="absolute left-0 top-0 size-full z-40">
+              <div className=" mt-24 px-5 sm:px-10 ">
+                <div className=" flex flex-col items-start text-center h-full">
+                <spam className="block font-orbitron hero-heading  text-white  text-center">
+                santsmcb
+                </spam>
+                <spa></spa>
+                <span className="mt-2  text-center font-general text-white text-sm md:text-md md:ml-10 lg:text-lg lg:ml-16">
+                  {"<"}Blending code, design, and 3D art <br/>
+                  into digital experiences.{">"}
+                </span>
+                <Button
+                id='watch'
+                title="Watch"
+                leftIcons={<TiLocationArrow/>}
+                containerClass="bg-tertiary flex-center gap-1"
+                />
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
     </div>
-  </div>;
+  );
 };
 
 export default Hero;
